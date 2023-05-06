7163793b620b21e230904b9e29669f2e21dcb2bf
Artem Bondar <artem.bondar@gmail.com>
2023-05-06 16:42:26 -0500

    parent-child

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,6 +1,72 @@
 import { test } from '@playwright/test'
 
-test('Locator syntax rules', async ({page}) => {
+test.beforeEach( async ({page}) => {
     await page.goto('http://localhost:4200')
-    // await page.locator('button')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+})
+
+test.skip('Locator syntax rules', async ({page}) => {
+    //by Tag Name
+    page.locator('input')
+
+    //by ID
+    page.locator('#inputEmail1')
+
+    //by Class name
+    page.locator('.input-full-width')
+
+    //by Attribute name
+    page.locator('[placeholder]')
+
+    //by Attribute name and value
+    page.locator('[placeholder="Email"]')
+
+    //by Class value
+    page.locator('[class="input-full-width size-medium shape-rectangle"]')
+
+    //combination of selectors
+    await page.locator('input[placeholder="Email"]').first().click()
+    page.locator('[placeholder="Email"][type="email"]')
+
+    //by partial text match
+    await page.locator(':text("Using")').click()
+
+    //by exact text match
+    await page.locator(':text-is("Using the Grid")').click()
+})
+
+test('Locatinth with getBy..', async ({page}) => {
+    await page.getByRole('textbox', {name: "Email"}).first().click()
+    await page.getByRole('button', {name: "Sign in", exact: true}).first().click()
+
+    await page.getByLabel('Email').first().click()
+
+    await page.getByPlaceholder('Jane Doe', {exact: true}).click()
+
+    await page.getByText('Using the Grid').click()
+
+    await page.getByTestId('SignIn').click()
+
+    await page.getByTitle('IoT Dashboard').click()
+})
+
+test('locating child elements', async ({page}) => {
+
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click()
+
+    await page.locator('nb-card').getByRole('button', {name: "Sign in"}).first().click() //also demo the last() method
+
+    await page.locator('nb-card').nth(3).getByRole('button').click()
+})
+
+test('locating parent elements', async ({page}) => {
+
+    await page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"}).click()
+    await page.locator('nb-card', {has: page.locator('#inputEmail1')}).getByRole('textbox', {name: "Email"}).click()
+
+    await page.locator('nb-card').filter({ hasText: "Basic form"}).getByRole('textbox', {name: "Email"}).click()
+    await page.locator('nb-card').filter({ has: page.locator('.status-danger')}).getByRole('textbox', {name: "Password"}).click()
+
+    await page.locator(':text-is("Using the Grid")').locator('..').click()
 })
