3ac0c65ce97ad54956f7aef529b0d4776f9875d9
Artem Bondar <artem.bondar@gmail.com>
2023-05-11 22:23:01 -0500

    lists and dropdowns

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -58,3 +58,36 @@ test.describe('Toaster page', () => {
 
     })
 })
+
+test('Lists and Dropdown', async ({page}) => {
+    await page.goto('http://localhost:4200')
+
+    const dorpDownMenu = page.locator('ngx-header nb-select')
+    await dorpDownMenu.click()
+
+    page.getByRole('list') // when the list has a UL tag
+    page.getByRole('listitem') //the the list has LI tag
+
+    const optionList = page.locator('nb-option-list nb-option')
+    await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"])
+    await optionList.filter({hasText: "Cosmic"}).click()
+    await expect(dorpDownMenu).toHaveText("Cosmic")
+    const header = page.locator('nb-layout-header')
+    await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)')
+
+    const colors = {
+        "Light": "rgb(255, 255, 255)",
+        "Dark": "rgb(34, 43, 69)",
+        "Cosmic": "rgb(50, 50, 89)",
+        "Corporate": "rgb(255, 255, 255)"
+    }
+
+    await dorpDownMenu.click()
+    
+    for(const color in colors){
+        await optionList.filter({hasText: color}).click()
+        await expect(header).toHaveCSS('background-color', colors[color])
+        if(color != "Corporate")
+            await dorpDownMenu.click()
+    }
+})
