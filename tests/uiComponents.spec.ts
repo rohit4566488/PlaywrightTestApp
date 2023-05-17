68292ee2fcdaa66e9ea1ff8ee2443dd04dd8fa37
Artem Bondar <artem.bondar@gmail.com>
2023-05-17 17:02:16 -0500

    dialog box and dooltip

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test'
 
+test.beforeEach( async({page}) => {
+    await page.goto('http://localhost:4200')
+})
 
 test.describe('Form Layouts page', () => {
     test.beforeEach( async ({page}) => {
-        await page.goto('http://localhost:4200')
         await page.getByText('Forms').click()
         await page.getByText('Form Layouts').click()
     })
@@ -39,7 +41,6 @@ test.describe('Form Layouts page', () => {
 
 test.describe('Toaster page', () => {
     test.beforeEach( async ({page}) => {
-        await page.goto('http://localhost:4200')
         await page.getByText('Modal & Overlays').click()
         await page.getByText('Toastr').click()
     })
@@ -60,7 +61,6 @@ test.describe('Toaster page', () => {
 })
 
 test('Lists and Dropdown', async ({page}) => {
-    await page.goto('http://localhost:4200')
 
     const dorpDownMenu = page.locator('ngx-header nb-select')
     await dorpDownMenu.click()
@@ -91,3 +91,29 @@ test('Lists and Dropdown', async ({page}) => {
             await dorpDownMenu.click()
     }
 })
+
+test('tooltips', async({page}) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Tooltip').click()
+
+    const toolTipCard = page.locator('nb-card', {hasText: 'Tooltip Placements'})
+    await toolTipCard.getByRole('button', {name: "Top"}).hover()
+
+    page.getByRole('tooltip') //if you have a role tooltip created for the tooltip
+    const tooltip = await page.locator('nb-tooltip').textContent()
+    expect(tooltip).toEqual('This is a tooltip')
+})
+
+test('dialog box', async({page}) => {
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+
+    page.on('dialog', dialog => {
+        expect(dialog.message()).toEqual('Are you sure you want to delete?')
+        dialog.accept()
+    })
+    await page.getByRole('table').locator('tr', {hasText: "mdo@gmail.com"}).locator('.nb-trash').click()
+    
+    await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
+})
+
