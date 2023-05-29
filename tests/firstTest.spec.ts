5d3e7c4848bf586abf08a46ad8045d453030b6c8
Artem Bondar <artem.bondar@gmail.com>
2023-05-29 15:20:38 -0500

    corrections

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -41,15 +41,15 @@ test('Locator syntax rules', async ({page}) => {
 
 test('Locatinth with getBy..', async ({page}) => {
     await page.getByRole('textbox', {name: "Email"}).first().click()
-    await page.getByRole('button', {name: "Sign in", exact: true}).first().click()
+    await page.getByRole('button', {name: "Sign in"}).first().click()
 
     await page.getByLabel('Email').first().click()
 
-    await page.getByPlaceholder('Jane Doe', {exact: true}).click()
+    await page.getByPlaceholder('Jane Doe').click()
 
     await page.getByText('Using the Grid').click()
 
-    await page.getByTestId('SignIn').click()
+    // await page.getByTestId('SignIn').click()
 
     await page.getByTitle('IoT Dashboard').click()
 })
