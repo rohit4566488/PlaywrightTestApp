34dc98f10e5f9a28e4746d42957dcfd92da7d006
Artem Bondar <artem.bondar@gmail.com>
2023-08-15 16:52:08 -0500

    faker

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager'
+import { faker } from '@faker-js/faker'
 
 test.beforeEach( async ({page}) => {
     await page.goto('http://localhost:4200')
@@ -17,9 +18,12 @@ test('navigation test', async ({page}) => {
 
 test('parametrized methods', async({page}) => {
     const pm = new PageManager(page)
+    const randomfullName = faker.person.fullName()
+    const randomEmail = `${randomfullName.replace(' ', '')}${faker.number.int({max: 1000})}@test.com`
+
     await pm.navigateTo().formLayoutsPage()
     await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'password', 'Option 1')
-    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox('John Smith', 'JohnSmith@test.com', false)
+    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox(randomfullName, randomEmail, false)
     await pm.navigateTo().datePickerPage()
     await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(5)
     await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 10)
