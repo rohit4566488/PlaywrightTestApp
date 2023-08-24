87ce309b40647a29458951ce3bdb6e48dbdfb378
Artem Bondar <artem.bondar@gmail.com>
2023-08-24 18:53:27 -0500

    reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,12 @@ export default defineConfig<TestOptions>({
   },
   fullyParallel: false,
   retries: 0,
-  reporter: 'html',
+  reporter: [
+    ['json', {outputFile: 'test-results/jsonReport.json'}],
+    ['junit', {outputFile: 'test-results/junitReport.xml'}],
+    ['allure-playwright']
+  ],
+  
 
   use: {
     globalsQaUrl: 'https://www.globalsqa.com/demo-site/draganddrop/',
